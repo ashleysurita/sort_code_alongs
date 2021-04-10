f83af63b73963ec5528319d51e031e77a071cfcd
Ashley <ashleywong678@gmail.com>
2021-04-10 19:27:20 -0400

    bubble sort code along

---
+++ bubble_sort_code_along.js
@@ -0,0 +1,17 @@
+// code along from https://www.youtube.com/watch?v=xli_FI7CuzA
+
+const bubbleSort = (arr) => {
+  let sorted = [...arr];
+  for (i = 1; i <= sorted.length; i++) {
+    for (j = 0; j < sorted.length; j++) {
+      if (sorted[j] > a[j + 1]) {
+        const a = sorted[j];
+        const b = sorted[j + 1];
+        sorted[j] = b;
+        sorted[j + 1] = a;
+        bubbleSort(sorted);
+      }
+    }
+  }
+  return sorted;
+};
