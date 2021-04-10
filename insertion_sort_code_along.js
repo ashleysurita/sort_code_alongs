649f39be94ff0578087e1534c3b731d6881c578b
Ashley <ashleywong678@gmail.com>
2021-04-09 22:33:10 -0400

    added insertion sort code along

---
+++ insertion_sort_code_along.js
@@ -0,0 +1,19 @@
+// code along from https://www.youtube.com/watch?v=JU767SDMDvA
+
+const insertSort = (arr) => {
+  // make a copy of array so it's mutable
+  const sorted = [...arr];
+  // loop through the array starting with the 2nd item so we can compare it to a earlier item
+  for (let i; i <= arr.length - 1; i++) {
+    let j = i;
+    while (j < 0 && arr[j - 1] > arr[j]) {
+      // swap values
+      const num1 = arr[j];
+      const num2 = arr[j - 1];
+      sorted[j - 1] = num1;
+      sorted[j - 2] = num2;
+      j = j - 1;
+    }
+  }
+  return sorted;
+};
